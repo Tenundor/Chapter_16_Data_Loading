5fee0e314ebe4f4e9a9c801d0c2b9cd0c5972771
tenundor@gmail.com <He5LA90m7a9T>
2020-09-20 21:02:44 +0300

    print max temperature

--- highs_lows.py
+++ highs_lows.py
@@ -4,5 +4,8 @@ filename = 'Data/Moscow weather 2019-2020.csv'
 with open(filename) as weather_csv:
     reader = csv.reader(weather_csv)
     header_row = next(reader)
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    for row in reader:
+        highs.append(row[12])
+
+print(highs)
