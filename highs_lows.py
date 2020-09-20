98820185cabccce84dc02ed8bd82a6ae2acaaf7e
tenundor@gmail.com <He5LA90m7a9T>
2020-09-20 20:42:06 +0300

    print index & column_header

--- highs_lows.py
+++ highs_lows.py
@@ -4,4 +4,5 @@ filename = 'Data/Moscow weather 2019-2020.csv'
 with open(filename) as weather_csv:
     reader = csv.reader(weather_csv)
     header_row = next(reader)
-    print(header_row)
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
