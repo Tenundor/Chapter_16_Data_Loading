f6f1f16150e76187889a1a7d7325b45e9fc248b8
tenundor@gmail.com <He5LA90m7a9T>
2020-09-21 11:33:20 +0300

    fill empty entries in data set

--- highs_lows.py
+++ highs_lows.py
@@ -1,11 +1,27 @@
 import csv
 
 filename = 'Data/Moscow weather 2019-2020.csv'
+
+
+def fill_empty_string(string, filler):
+    """Assigns to the "String" variable the value of the "filler", if "string" is empty"""
+    if string == '':
+        return filler
+    else:
+        return string
+
+
 with open(filename) as weather_csv:
     reader = csv.reader(weather_csv)
     header_row = next(reader)
     highs = []
+    high_previous = 0
     for row in reader:
-        highs.append(row[12])
+        filled_high = fill_empty_string(row[12], high_previous)
+        highs.append(filled_high)
+        high_previous = filled_high
+
 
 print(highs)
+
+
