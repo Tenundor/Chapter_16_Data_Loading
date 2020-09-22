02f1b0ed0db2fbf3772f54afa0efbca7b3d834d1
tenundor@gmail.com <He5LA90m7a9T>
2020-09-22 19:43:20 +0300

    add exception

--- highs_lows.py
+++ highs_lows.py
@@ -12,19 +12,19 @@ with open(filename) as weather_csv:
     dates, highs, lows = [], [], []
 
     for row in reader:
-        high_temp = row[12]
-        low_temp = row[14]
-        if high_temp == '' or low_temp == '':
-            continue
-        date_string = row[5]
-        current_date = datetime.strptime(date_string, "%Y-%m-%d")
-        dates.append(current_date)
-
-        high_temp_float = float(high_temp)
-        highs.append(high_temp_float)
-
-        low_temp_float = float(low_temp)
-        lows.append(low_temp_float)
+        try:
+            high_temp = row[12]
+            low_temp = row[14]
+            date_string = row[5]
+            current_date = datetime.strptime(date_string, "%Y-%m-%d")
+            high_temp_float = float(high_temp)
+            low_temp_float = float(low_temp)
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high_temp_float)
+            lows.append(low_temp_float)
 
 
 print(header_row)
