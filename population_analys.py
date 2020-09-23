165b3f214ea7add94c23c2f54b1616c4d0aa3fff
tenundor@gmail.com <He5LA90m7a9T>
2020-09-23 16:36:17 +0300

    add world map

--- population_analys.py
+++ population_analys.py
@@ -1,5 +1,8 @@
 import json
 
+
+from country_codes import get_country_code
+
 filename = 'Data/population_data.json'
 with open(filename) as pop_file:
     pop_data = json.load(pop_file)
@@ -8,4 +11,8 @@ for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        print(country_name + ": " + str(population))
+        country_code = get_country_code(country_name)
+        if country_code:
+            print(country_code + ": " + str(population))
+        else:
+            print('ERROR - ' + country_name)
---
+++ world_map.py
@@ -0,0 +1,9 @@
+from pygal.maps.world import World
+
+
+wm = World()
+wm.title = 'North, Central, and South America'
+
+wm.add('North America', ['ca', ])
+
+wm.add()
